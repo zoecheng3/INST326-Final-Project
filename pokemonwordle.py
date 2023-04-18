940fa64ae61c0429a6b78db7f1b820a398786e13
Jonner909 <99283670+Jonner909@users.noreply.github.com>
2023-04-17 21:55:01 -0400

    Create pokemonwordle.py

---
+++ pokemonwordle.py
@@ -0,0 +1,12 @@
+def main():
+  
+def get_rand_poke(poke_list):
+  
+def guess_poke():
+  
+def end_game(guess, poke, correctly_guessed):
+  if correctly_guessed:
+    print(f"Correct! The Pokemon is {poke}")
+  else:
+    print(f"Game Over! The correct Pokemon was {poke}")
+  
