def8fec384dee6f7dd1e83754235e18151c14523
Jonner909 <99283670+Jonner909@users.noreply.github.com>
2023-04-30 20:28:53 -0400

    Update pokemonwordle.py

--- pokemonwordle.py
+++ pokemonwordle.py
@@ -14,20 +14,34 @@ class Pokemon:
       print('Youve chosen a fire pokemon')
 
 
-def main():
-  
+def play_game():
+  correct_answer = random.choice()
+  lives = 5
+  while lives > 0:
+    guess = input("Guess a Pokemon: ")
+    if guess = correct answer:
+      print(f"Correct! The Pokemon is {correct_answer}")
+    else:
+      lives -=1
+      print(f"Incorrect, you have {lives} guesses left")
+    if lives == 0:
+      print(f"You ran out of guesses, the Pokemon was {correct_answer}")
+  play_again = input("Do you want to play again? (y/n)").lower()
+    if play_again == "y":
+      play_game()
+    else:
+      print("Thanks for playing!")
+      
+                     
+                     
+                     
+                     
 def get_rand_poke(poke_list):
   return random.choice(poke_list)
   
-def guess_poke(self):
   
   
-def end_game(guess, poke, correctly_guessed):
-  if correctly_guessed:
-    print(f"Correct! The Pokemon is {poke}")
-  else:
-    print(f"Game Over! The correct Pokemon was {poke}")
-  
+
  
 if __name__ == '__main__':
   main()
