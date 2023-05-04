239603dd345f520b95fccf0310d4fa9d65eda719
Jonner909 <99283670+Jonner909@users.noreply.github.com>
2023-05-04 15:51:52 -0400

    Update pokemonwordle.py

--- pokemonwordle.py
+++ pokemonwordle.py
@@ -1,56 +1,62 @@
-from argparse import ArgumentParser
+#This will be the test code for the function
+
 import pandas as pd
-import random
-import re
-import sys
-
-test test
-
-def play_game():
-  correct_answer = random.choice() #when i think about it, i think this variable should be equal to the function that chooses a random pokmon from the dataframe
-  lives = 5
-  while lives > 0:
-    guess = input("Guess a Pokemon: ")
-    if guess = correct answer:
-      print(f"Correct! The Pokemon is {correct_answer}")
-    else:
-      lives -=1
-      print(f"Incorrect, you have {lives} guesses left")
-    if lives == 0:
-      print(f"You ran out of guesses, the Pokemon was {correct_answer}")
-  play_again = input("Do you want to play again? (y/n)").lower()
-    if play_again == "y":
-      play_game()
-    else:
-      print("Thanks for playing!")
-      
-                     
-                     
-                     
-                  
-  
-  
-
- 
-if __name__ == '__main__':
-  
-  
-  def pokemon_wordle(file):
+import random as rand
+def pokemon_wordle(file):
   with open('pokemon.csv', encoding = 'utf -8') as f:
     df = pd.read_csv(f)
     new_df = df.drop(['HP','Total', 'Attack', 'Defense', 'Sp. Def', 'Speed', 'Sp. Atk' ], 
                  axis = 1)
-    for name in new_df['Name']:
-      game_choice = rand.choice(new_df['Name'])
+    
+    poke_choice = list(new_df['Name'])
+
+    for name in poke_choice:
+      game_choice = rand.choice(poke_choice)
   return game_choice
-
-
-  test = pokemon_wordle
 
     
 
 
 
+def play_game():
+  
+  testf = 'pokemon.csv'
+  correct_answer = pokemon_wordle(testf)
+  print(f'Correct answer: {correct_answer}')
+  lives = 5
 
-  main()
+  
+  while lives > 0:
+      answer = input('Guess the pokemon: ') 
+      if answer == correct_answer:
+        print(f"Correct! The Pokemon is {correct_answer}")
+        play_again = input("Do you want to play again? (y/n)").lower()
+        if play_again == "y":
+            play_game()
+        else:
+            break ;print("Thanks for playing!")
+            
+      else:
+        lives -=1
+        print(f"Incorrect, you have {lives} guesses left")
+        
+      if lives == 0 and answer != correct_answer:
+        print(f"You ran out of guesses, the Pokemon was {correct_answer}")
+        play_again = input("Do you want to play again? (y/n)").lower()
+        if play_again == "y":
+            play_game()
+        else:
+            break ;print("Thanks for playing!")
+      
+      
+
+
+
+                     
+                     
+                     
+            if __name__ == '__main__':
+  
+  
+  
   
